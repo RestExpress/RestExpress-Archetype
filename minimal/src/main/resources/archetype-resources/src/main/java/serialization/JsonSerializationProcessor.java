f003d1a6f7fde73bb91fe5447a7622950ccc2f0d
Todd Fredrich <tfredrich@gmail.com>
2013-02-08 16:12:04 -0700

    Updated to use RestExpress 0.9.0-SNAPSHOT, which uses Jackson for JSON processing.

--- minimal/src/main/resources/archetype-resources/src/main/java/serialization/JsonSerializationProcessor.java
+++ minimal/src/main/resources/archetype-resources/src/main/java/serialization/JsonSerializationProcessor.java
@@ -3,24 +3,18 @@
 #set( $symbol_escape = '\' )
 package ${package}.serialization;
 
-import java.util.Date;
-
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.strategicgains.restexpress.serialization.json.DefaultJsonProcessor;
-import com.strategicgains.restexpress.serialization.json.GsonTimestampSerializer;
-import com.strategicgains.util.date.DateAdapterConstants;
 
 public class JsonSerializationProcessor
 extends DefaultJsonProcessor
 {
-	public JsonSerializationProcessor()
+	@Override
+    protected void initializeModule(SimpleModule module)
     {
-	    super(
-	    	new GsonBuilder()
-			.disableHtmlEscaping()
-			.registerTypeAdapter(Date.class, new GsonTimestampSerializer())
-			.setDateFormat(DateAdapterConstants.TIMESTAMP_OUTPUT_FORMAT)
-			.create()
-	    );
+	    super.initializeModule(module);
+	    
+	    //TODO: add your own [de]serializers here.
+	    //module.addDeserializer(type, deser);
     }
 }
