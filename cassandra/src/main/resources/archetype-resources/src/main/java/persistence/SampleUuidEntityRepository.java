8b095c8ee859e4a0624cdaeb8d4ba1c02b9fdf97
Todd Fredrich <tfredrich@gmail.com>
2014-01-27 08:46:29 -0700

    Changed SampleUuidEntityRepository to extend CassandraTimestampedEntityRepository (vs. CassandraEntityRepository) to get automatic createdAt and UpdatedAt behavior.

--- cassandra/src/main/resources/archetype-resources/src/main/java/persistence/SampleUuidEntityRepository.java
+++ cassandra/src/main/resources/archetype-resources/src/main/java/persistence/SampleUuidEntityRepository.java
@@ -9,10 +9,10 @@ import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.strategicgains.repoexpress.cassandra.CassandraEntityRepository;
+import com.strategicgains.repoexpress.cassandra.CassandraTimestampedEntityRepository;
 
 public class SampleUuidEntityRepository
-extends CassandraEntityRepository<SampleUuidEntity>
+extends CassandraTimestampedEntityRepository<SampleUuidEntity>
 {
 	private static final String UPDATE_CQL = "update %s set updatedat = ? where %s = ?";
 	private static final String CREATE_CQL = "insert into %s (%s, createdat, updatedat) values (?, ?, ?)";
